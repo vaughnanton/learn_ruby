d0de182ed713637e158c49c41a333779c323f547
Vaughn Antonyan <vaughnanton@gmail.com>
2017-05-20 23:23:19 -0700

    Fixed failures

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,19 @@
-#write your code here
+def add(num0, num1)
+  num0+num1
+end
+
+def subtract(num0,num1)
+  num0-num1
+end
+
+def multiply(num0,num1)
+  num0*num1
+end
+
+def sum(num)
+  if num == []
+    0
+  else
+    num.inject(0){|sum,x| sum + x}
+end
+end
