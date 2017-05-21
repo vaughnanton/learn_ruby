6fa335f8431c403a38c5ef7da66bde16764b5101
Vaughn Antonyan <vaughnanton@gmail.com>
2017-05-21 00:41:16 -0700

    Fixed failures

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
-#write your code here
+def translate_single(s)
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  if vowels.include?(s[0])
+    "#{s}ay"
+  else
+    i = 0
+    while i < s.length
+      if vowels.include?(s[i])
+        break
+      elsif s[i,2] == 'qu'
+        i += 2
+        break
+      end
+      i += 1
+    end
+    prefix = s[0,i]
+    suffix = s[i, s.length - i]
+    "#{suffix}#{prefix}ay"
+  end
+end
+
+def translate(s)
+  s.split.map { |word| translate_single(word) }.join(' ')
+end
