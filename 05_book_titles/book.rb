9c73989b719e09bc82fca9edf65042b3aeede9ed
Vaughn Antonyan <vaughnanton@gmail.com>
2017-05-21 01:07:04 -0700

    Fixed failures

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+  def title
+    @title
+  end
+
+  def title=(title)
+    @title=titleize(title)
+  end
+
+  private
+    def titleize(title)
+      little_words = %w{and but or for the a an in of}
+      title.capitalize.split.map{|w| little_words.include?(w) ? w : w.capitalize}.join(" ")
+    end
 end
