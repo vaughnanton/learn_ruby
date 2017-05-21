8135aa5a3040b5bcfbb93a3a5c7c011d362023b9
Vaughn Antonyan <vaughnanton@gmail.com>
2017-05-21 01:25:19 -0700

    Fixed failures

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,26 @@
 class Timer
-  #write your code here
+  def initialize
+  	@seconds = 0
+  end
+
+  def seconds
+  	@seconds
+  end
+
+  def seconds=value
+  	@seconds = value.to_i
+  end
+
+  def time_string
+  	h = (@seconds/3600)
+  	m = (@seconds%3600)/60
+  	s = (@seconds%3600)%60
+  	"#{self.padded(h)}:#{self.padded(m)}:#{self.padded(s)}"
+  end
+
+  def padded value
+  	value = value.to_s
+  	'0'*(2-value.length)+value
+  end
+
 end
